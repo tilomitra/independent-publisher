58b75d51968ee761ffd53f636b6511881142705c
Tilo <tilomitra@gmail.com>
2014-09-01 17:03:04 -0400

    add Typekit font support

--- header.php
+++ header.php
@@ -19,6 +19,8 @@
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 	<![endif]-->
 	<?php wp_head(); ?>
+	<script type="text/javascript" src="//use.typekit.net/dte0tzk.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 </head>
 
 <body <?php body_class(); ?> itemscope="itemscope" itemtype="http://schema.org/WebPage">
